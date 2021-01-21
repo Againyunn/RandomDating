a73890ddebf7c2f62523fb57f8abc25cb2e3b4d2
jaeyun <rangyun36@gmail.com>
2021-01-21 14:59:40 +0900

    version 20210121

--- user/urls.py
+++ user/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
         path('form/', views.form, name='form'),
         path('main/', views.main, name='main'),
         path('login/', views.login, name='login'),
+        path('calender/', views.calender, name='calender'),
+        path('<int:question_id>/' , views.detail, name="detail"), #숫자 형태로 입력받는 경우 모두 veiws.detail을 실행
 ]
--- user/views.py
+++ user/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render, redirect
+from django.http.response import HttpResponse
 from . models import User
 
 # Create your views here.
+def detail(request, question_id):
+    return HttpResponse(f"You're {question_id} looking at {question_id}")
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s %s."
+    return HttpResponse(response % (question_id, question_id)) # % 을 넣는다는 의미 : %s 의 변수에 ( )안의 값을 입력하라는 의미.
+
+def calender(request):
+    context={"loop_1" : range(1,32) } #dictionary로 지정하여 loop_1 과 loop_2로 넣어서 render를 통해 넘길 수 있다.
+    return render(request, 'calender.html', context) # return render(request, 불러올 파일, 전송할 데이터)의 형태
+
+
+def index(request):
+    context={"index_1" : "코딩온" , "loop_1" : range(10) , "loop_2" : range(5, 10) } #dictionary로 지정하여 loop_1 과 loop_2로 넣어서 render를 통해 넘길 수 있다.
+    return render(request, 'index.html', context) # return render(request, 불러올 파일, 전송할 데이터)의 형태
+
 def login(request):
     if request.method == "POST" :
         userid = request.POST.get('userid')
