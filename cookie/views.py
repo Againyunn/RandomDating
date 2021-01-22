14a0e1602edf62267f351e69b8803043d494f409
Againyunn <rangyun36@gmail.com>
2021-01-22 17:36:56 +0900

    cookie 암호화

--- cookie/views.py
+++ cookie/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from django.contrib.auth.hashers import make_password
 
 # Create your views here.
-def index(req):
-    return HttpResponse("<h1>cookie</h1>")
+def index(request):
+    if request.method=="POST":
+        pw = request.POST.get('password', '')
+        print(pw)
+        res = render(request, "signup.html")
+        res.set_cookie('pw', make_password(pw))
+        return res
+
+    return render(request, "signup.html")
